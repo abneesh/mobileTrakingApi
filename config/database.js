bf3b5ca86c807f3a70cd9bc1fe5df076eb7cde2e
abneesh <abneesh@users.noreply.github.com>
2017-08-15 08:53:07 +0530

    commit
    commit db

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
  * Created by abneeshdubey on 27/07/17.
  */
 var mongoose = require('mongoose');
-var config = require('./index').database;
-mongoose.Promise = global.Promise;
+// var config = require('./index').database;
+// mongoose.Promise = global.Promise;
 
 var option={
     user:'',
@@ -17,5 +17,5 @@ mongoose.connection.on('error', function (err) {
     console.log('Could not connect to mongo server');
     console.log(err)
 });
-mongoose.connect(config.host);
-//mongoose.connect('mongodb://abneesh:abneesh@123@ds129183.mlab.com:29183/user_mobile_db');
+// mongoose.connect(config.host);
+mongoose.connect('mongodb://ok:ok@ds149481.mlab.com:49481/abneeshdb');
